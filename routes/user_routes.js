fa2b11c207ae50c08a9547e30da0e7a161fe1845
basti <Seb.wehner@googlemail.com>
2022-04-02 20:27:40 +0200

    refactored all user routes to be async

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -9,148 +9,159 @@ const authenticateToken = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-    const { username, email, password } = req.body;
+router.post("/register", async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
 
-    const salt = bcrypt.genSaltSync( parseInt(process.env.SALT_ROUNDS) );
-    const hash = bcrypt.hashSync(password, salt);
-
-    const db = dbConnection.getDb();
-    res.setHeader('Content-Type', 'application/json');
+        const salt = bcrypt.genSaltSync( parseInt(process.env.SALT_ROUNDS) );
+        const hash = bcrypt.hashSync(password, salt);
     
-    // check that user with this email does not exist
-    db.collection("app-users").findOne({email})
-        .then( existing_user => {
-            if (!existing_user){
-                db.collection("app-users")
-                .insertOne({username, 
-                        email, 
-                        password_hash: hash, 
-                        email_validated: false,
-                        registered_at: new Date(), 
-                        icon_path: "",
-                        position: "",
-                        bio: "",
-                        tokens: [],
-                        team_ids: [],
-                        boards: [],
-                        personal_notes: []
-                    })
-                    .then( insert_result => {
-                        const token = generateAccessToken(insert_result.insertedId.toString());
-                        db.collection("app-users").findOneAndUpdate({email}, {$set: {"tokens": [token]}});
-                        res.send(JSON.stringify({username, email, token, icon_path: ""}));
-                    })
-                    .catch( error => {
-                        console.error(error);
-                        res.status(403).send(JSON.stringify({message: "Unable to generate token from user_id"}));
-                    });
-
-            } else {
-                res.status(401).send(JSON.stringify({message: "User with that email does already exist."}));
-            }
-        })
-        .catch(error => {
-            console.error(err);
-            res.status(401).send(JSON.stringify({err}));
-        });
-});
-
-router.post("/login", (req, res) => {
-    const { email, password } = req.body;   
-    const db = dbConnection.getDb(); 
-
-    res.setHeader('Content-Type', 'application/json');
-    
-    db.collection("app-users").findOne({email})
-        .then( existing_user => {
-            if (bcrypt.compareSync(password, existing_user.password_hash)){
-                const token = generateAccessToken(existing_user._id.toString());
-                db.collection("app-users").updateOne({email}, {$push: {tokens: token}});
-                res.send(JSON.stringify({ username: existing_user.username, email: existing_user.email, token, icon_path: existing_user.icon_path}));
-            } else {
-                res.status(401).send({message: "Incorrect password."});
-            }
-        })
-        .catch( error => {
-            res.status(401).send({message: `Error occured: ${error}`});
-        })
-});
-
-router.post("/logout", authenticateToken, (req, res) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader.split(" ")[1];
-    res.setHeader('Content-Type', 'application/json');
-    const db = dbConnection.getDb();
-    db.collection("app-users").updateOne({_id: new mongo.ObjectId(req.decoded_token.user_id)}, { $pull: {tokens: token} })
-        .then( update_result => {
-            console.log("update result: ". update_result);
-            res.send(JSON.stringify({message: "Successfully logged out"}));
-        })
-        .catch( error=> {
-            res.status(401).send(JSON.stringify({message: "Unable to logout user"}));
-        })
-});
-
-router.get("/me", authenticateToken, (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const db = dbConnection.getDb();
-    db.collection("app-users").findOne({_id: new mongo.ObjectId(req.decoded_token.user_id)})
-        .then( user => {
-            if (user){
-                res.send({
-                    username: user.username,
-                    email: user.email,
-                    email_validated: user.email_validated,
-                    icon_path: user.icon_path
-                });    
-            } else {
-                res.status(401).send(JSON.stringify({message: "Something went horribly wrong"}));
-            }
-        })
-        .catch( error => {
-            res.status(401).send(JSON.stringify({message: `Error occured: ${error}`}));
-        });
-});
-
-router.get("/profile", authenticateToken, (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const db = dbConnection.getDb();
-    db.collection("app-users").findOne({_id: new mongo.ObjectId(req.decoded_token.user_id)})
-        .then( user => {
-            if (user) {
-                res.send({
-                    registered_at: new Date(user.registered_at).toLocaleString(),
-                    bio: user.bio || "",
-                    position: user.position || "",
-                    boards: user.boards || [],
-                    personal_notes: user.personal_notes || [],
-                    team_ids: user.team_ids || []
+        const db = dbConnection.getDb();
+        res.setHeader('Content-Type', 'application/json');
+        
+        // check that user with this email does not exist
+        let existing_user = await db.collection("app-users").findOne({email});
+        if (!existing_user){
+            db.collection("app-users")
+            .insertOne({username, 
+                    email, 
+                    password_hash: hash, 
+                    email_validated: false,
+                    registered_at: new Date(), 
+                    icon_path: "",
+                    position: "",
+                    bio: "",
+                    tokens: [],
+                    team_ids: [],
+                    boards: [],
+                    personal_notes: []
+                })
+                .then( insert_result => {
+                    const token = generateAccessToken(insert_result.insertedId.toString());
+                    db.collection("app-users").findOneAndUpdate({email}, {$set: {"tokens": [token]}});
+                    res.send(JSON.stringify(
+                        {
+                            username, 
+                            email, 
+                            token, 
+                            icon_path: ""
+                        }));
+                })
+                .catch( error => {
+                    console.error(error);
+                    res.status(403).send(JSON.stringify({message: "Unable to generate token from user_id"}));
                 });
-            } else {
-                res.status(401).send(JSON.stringify({message: "No user with this id is known."}));
-            }
-        })
-        .catch( error => {
-            res.status(401).send(JSON.stringify({message: `Error occured: ${error}`}));
-        });
+
+        } else {
+            res.status(401).send(JSON.stringify({message: "User with that email does already exist."}));
+        }
+    } catch (error) {
+        console.error(err);
+        res.status(401).send(JSON.stringify({err}));
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;   
+        const db = dbConnection.getDb(); 
+    
+        res.setHeader('Content-Type', 'application/json');
+        
+        let existing_user = await db.collection("app-users").findOne({email});
+        if (bcrypt.compareSync(password, existing_user.password_hash)){
+            const token = generateAccessToken(existing_user._id.toString());
+            db.collection("app-users").updateOne({email}, {$push: {tokens: token}});
+            res.send(JSON.stringify(
+                {
+                    username: existing_user.username, 
+                    email: existing_user.email, 
+                    token, 
+                    icon_path: existing_user.icon_path
+                }));
+        } else {
+            res.status(401).send({message: "Incorrect password."});
+        }
+    } catch (error) {
+        res.status(401).send({message: `Error occured: ${error}`});
+    }
+});
+
+router.post("/logout", authenticateToken, async (req, res) => {
+    try{
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(" ")[1];
+        res.setHeader('Content-Type', 'application/json');
+        const db = dbConnection.getDb();
+        
+        let update_result = await db.collection("app-users").updateOne({_id: new mongo.ObjectId(req.decoded_token.user_id)}, { $pull: {tokens: token} });
+        console.log("update result: ", update_result);
+        res.send(JSON.stringify({message: "Successfully logged out"}));
+    } catch (error) {
+        res.status(401).send(JSON.stringify({message: "Unable to logout user"}));
+    }
+});
+
+router.get("/me", authenticateToken, async (req, res) => {
+    try{
+        res.setHeader('Content-Type', 'application/json');
+        const db = dbConnection.getDb();
+        let user = await db.collection("app-users").findOne({_id: new mongo.ObjectId(req.decoded_token.user_id)});
+        if (user){
+            res.send({
+                username: user.username,
+                email: user.email,
+                email_validated: user.email_validated,
+                icon_path: user.icon_path
+            });
+        } else {
+            res.status(401).send(JSON.stringify({message: "Something went horribly wrong"}));
+        }
+    } catch (error){
+        res.status(401).send(JSON.stringify({message: `Error occured: ${error}`}));
+    }
+});
+
+router.get("/profile", authenticateToken, async (req, res) => {
+    try{
+        res.setHeader('Content-Type', 'application/json');
+        const db = dbConnection.getDb();
+        let user = await db.collection("app-users").findOne({_id: new mongo.ObjectId(req.decoded_token.user_id)});
+        if (user) {
+            res.send({
+                username: user.username,
+                email: user.email,
+                email_validated: user.email_validated,
+                icon_path: user.icon_path,
+                registered_at: new Date(user.registered_at).toLocaleString(),
+                bio: user.bio || "",
+                position: user.position || "",
+                boards: user.boards || [],
+                personal_notes: user.personal_notes || [],
+                team_ids: user.team_ids || []
+            });
+        } else {
+            res.status(401).send(JSON.stringify({message: "No user with this id is known."}));
+        }
+    } catch (error) {
+        res.status(401).send(JSON.stringify({message: `Error occured: ${error}`}));
+    }
 })
 
-router.post("/update_profile", authenticateToken, (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const { username, bio, position } = req.body;
-    const db = dbConnection.getDb();
-    db.collection("app-users").findOneAndUpdate(
-        {_id: new mongo.ObjectId(req.decoded_token.user_id)},
-        {$set: { username: username, bio: bio, position: position }})
-        .then( old_doc => {
-            // console.log("old doc: ", old_doc)
-            res.send(JSON.stringify({message: "update successful!"}));
-        })
-        .catch( err => {
-            // console.error("updating profile: ", err);
-            res.status(401).send(JSON.stringify({message: `Error occured ${err}`}));
-        });
+router.post("/update_profile", authenticateToken, async (req, res) => {
+    try {
+        res.setHeader('Content-Type', 'application/json');
+        const { username, bio, position } = req.body;
+        const db = dbConnection.getDb();
+        let old_doc = await db.collection("app-users").findOneAndUpdate({
+            _id: new mongo.ObjectId(req.decoded_token.user_id)},
+            {$set: { username: username, bio: bio, position: position }});
+        // console.log("old doc: ", old_doc)
+        res.send(JSON.stringify({message: "update successful!"}));
+    } catch (error){
+        // console.error("updating profile: ", err);
+        res.status(401).send(JSON.stringify({message: `Error occured ${err}`}));
+    }
 })
 
 // TEST ROUTE FOR DB ACCESS
